5c4776e042064c4609a40974712835408853ec67
Asa DeWeese <deweesa@Asas-MacBook-Air.local>
2024-01-07 11:27:12 -0800

    Simple landing page implemented

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ function App() {
   return (
     <div className="App">
       <h1> Tetris! </h1>
+      <div className="Buttons">
+        <button onClick={sayClicked}>Play</button>
+        <button onClick={sayClicked}>Help</button>
+      </div>
+      <p>
+        High Scores (implementing later)
+      </p>
     </div>
   );
+}
+
+function sayClicked() {
+  alert('You clicked me');
 }
 
 export default App;
